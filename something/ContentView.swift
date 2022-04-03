7bdb0c9121eaf2f6af8e870ce0dab2703dbc6a3d
dj-viking <anders.swedishviking@gmail.com>
2022-04-03 10:33:02 -0400

    Initial Commit

---
+++ something/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  something
+//
+//  Created by Anders Ackerman on 4/3/22.
+//  Copyright © 2022 Anders Ackerman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
